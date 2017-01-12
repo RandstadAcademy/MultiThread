08b4b5fd8d45c819465083a3a1b56315f9b47c4c
Aula Randstad <Aula Randstad@PC2211>
2017-01-12 14:50:03 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Multithreading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdGetWeb = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.cmdGetWebAsync = new System.Windows.Forms.Button();
+            this.lblWork = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdGetWeb
+            // 
+            this.cmdGetWeb.Location = new System.Drawing.Point(578, 12);
+            this.cmdGetWeb.Name = "cmdGetWeb";
+            this.cmdGetWeb.Size = new System.Drawing.Size(106, 23);
+            this.cmdGetWeb.TabIndex = 0;
+            this.cmdGetWeb.Text = "GetWeb";
+            this.cmdGetWeb.UseVisualStyleBackColor = true;
+            this.cmdGetWeb.Click += new System.EventHandler(this.cmdGetWeb_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 12);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(539, 284);
+            this.txtContent.TabIndex = 1;
+            // 
+            // cmdGetWebAsync
+            // 
+            this.cmdGetWebAsync.Location = new System.Drawing.Point(578, 57);
+            this.cmdGetWebAsync.Name = "cmdGetWebAsync";
+            this.cmdGetWebAsync.Size = new System.Drawing.Size(106, 23);
+            this.cmdGetWebAsync.TabIndex = 2;
+            this.cmdGetWebAsync.Text = "GetWeb Async";
+            this.cmdGetWebAsync.UseVisualStyleBackColor = true;
+            this.cmdGetWebAsync.Click += new System.EventHandler(this.cmdGetWebAsync_Click);
+            // 
+            // lblWork
+            // 
+            this.lblWork.AutoSize = true;
+            this.lblWork.Location = new System.Drawing.Point(578, 135);
+            this.lblWork.Name = "lblWork";
+            this.lblWork.Size = new System.Drawing.Size(44, 13);
+            this.lblWork.TabIndex = 3;
+            this.lblWork.Text = "NoData";
+            this.lblWork.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 308);
+            this.Controls.Add(this.lblWork);
+            this.Controls.Add(this.cmdGetWebAsync);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.cmdGetWeb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdGetWeb;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button cmdGetWebAsync;
+        private System.Windows.Forms.Label lblWork;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multithreading
+{
+    public partial class Form1 : Form
+    {
+        private Uri _WebAddress = new Uri( "http://www.libero.it");
+        SynchronizationContext _SynCtx = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _SynCtx = SynchronizationContext.Current;
+        }
+
+        private void SetText(byte[] bytes)
+        {
+            txtContent.Text = "";
+            txtContent.Text = System.Text.ASCIIEncoding.ASCII.GetString( bytes);
+        }
+        private void SetText( string text)
+        {
+            txtContent.Text = "";
+            txtContent.Text = text;
+        }
+
+        private void cmdGetWeb_Click(object sender, EventArgs e)
+        {
+            WebClient wcGet = new WebClient();
+            byte[] wcBytes = wcGet.DownloadData(this._WebAddress);
+
+            txtContent.Text = System.Text.ASCIIEncoding.ASCII.GetString(wcBytes);
+        }
+
+        private async void cmdGetWebAsync_Click(object sender, EventArgs e)
+        {
+
+            #region Asynchronous Method 1 - Legacy
+            //Thread Th = new Thread(GetWebContent);
+            //Th.Start();
+            #endregion
+
+            #region Asynchronous Method 2
+            //Task<string> Th1 = new Task<string>(GetWebContent2);
+
+            //Task T2 = Th1.ContinueWith((previousTask) =>
+            //    {
+            //        txtContent.Text = previousTask.Result;
+
+            //    }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //Th1.Start();
+            #endregion
+
+
+            WebClient wcGet = new WebClient();
+
+            #region Asynchronous Method 3
+            //wcGet.DownloadDataAsync( this._WebAddress);
+            //wcGet.DownloadDataCompleted += WcGet_DownloadDataCompleted;
+            #endregion
+
+            #region Asynchronous Mehtod 4
+            byte[] wcBytes = await wcGet.DownloadDataTaskAsync(this._WebAddress);
+            SetText( wcBytes);
+            #endregion
+
+        }
+
+        private void GetWebContent()
+        {
+            _SynCtx.Send( SetLabel, "Downloading");
+            WebClient wcGet = new WebClient();
+            byte[] wcBytes = wcGet.DownloadData(this._WebAddress);
+
+            _SynCtx.Send( SetText, wcBytes);
+
+            _SynCtx.Send(SetLabel, "Downloaded");
+        }
+        private string GetWebContent2()
+        {
+            WebClient wcGet = new WebClient();
+            byte[] wcBytes = wcGet.DownloadData(this._WebAddress);
+
+            return System.Text.ASCIIEncoding.ASCII.GetString( wcBytes);
+        }
+
+        private void SetLabel(object state)
+        {
+            lblWork.Text = (string)state;
+        }
+
+        private void SetText(object state)
+        {
+            SetText( (byte[])state);
+        }
+
+        private void WcGet_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            lock (txtContent)
+            {
+                SetText(e.Result);
+            }
+        }
+    }
+}
